106674bf55e44644051151e6e9ff64093910bb1b
Shayne Dsouza <shaynedsouza@hotmail.com>
2023-04-08 23:56:45 +0530

    string movement

--- Assets/Scripts/FishRodHandler.cs
+++ Assets/Scripts/FishRodHandler.cs
@@ -7,16 +7,26 @@ using UnityEngine;
 public class FishRodHandler : MonoBehaviour
 {
     bool canInteract = false;
-    [SerializeField] float maxXRotation = 10f;
-    [SerializeField] float maxYRotation = 40f;
 
-    // Range for rotation of the rod
-    Vector2 xRange, yRange;
+    [Header("Rod settings")]
+    [SerializeField] float maxXRotationRod = 10f;
+    [SerializeField] float maxYRotationRod = 40f;
+
+
+    [Header("String settings")]
+    [SerializeField] float maxLocalXOffsetString = 3f;
+    [SerializeField] float maxLocalZOffsetString = 1f;
+
+
+    // Range for rotation of the rod and position of the string
+    Vector2 xRodRange, yRodRange;
+    Vector2 xLocalStringRange, zLocalStringRange;
 
 
     Vector3 myRotation;
     InputHandler inputHandler;
     FishRodStringRenderer fishRodStringRenderer;
+    Transform lastPoint;
 
 
     private void OnEnable()
@@ -40,11 +50,22 @@ public class FishRodHandler : MonoBehaviour
         fishRodStringRenderer = GetComponent<FishRodStringRenderer>();
 
         //Getting the rotation range for the fish rod
-        xRange.x = myRotation.x + maxXRotation;
-        xRange.y = myRotation.x - maxXRotation;
+        xRodRange.x = myRotation.x + maxXRotationRod;
+        xRodRange.y = myRotation.x - maxXRotationRod;
 
-        yRange.x = myRotation.y - maxYRotation;
-        yRange.y = myRotation.y + maxYRotation;
+        yRodRange.x = myRotation.y - maxYRotationRod;
+        yRodRange.y = myRotation.y + maxYRotationRod;
+
+
+
+        //Getting the position range for the string
+
+        lastPoint = fishRodStringRenderer.lastPoint;
+        xLocalStringRange.x = lastPoint.localPosition.x - maxLocalXOffsetString;
+        xLocalStringRange.y = lastPoint.localPosition.x + maxLocalXOffsetString;
+
+        zLocalStringRange.x = lastPoint.localPosition.z - maxLocalZOffsetString;
+        zLocalStringRange.y = lastPoint.localPosition.z + maxLocalZOffsetString;
 
     }
 
@@ -54,9 +75,14 @@ public class FishRodHandler : MonoBehaviour
         if (!canInteract) return;
 
         //Remapping the ranges and setting the rotation
-        float xRot = Map(inputHandler.mousePosition.y, 0f, Screen.height, xRange.x, xRange.y);
-        float yRot = Map(inputHandler.mousePosition.x, 0f, Screen.width, yRange.x, yRange.y);
-        transform.rotation = Quaternion.Euler(xRot, yRot, transform.rotation.z);
+        float xRot = Map(inputHandler.mousePosition.x, 0f, Screen.width, yRodRange.x, yRodRange.y);
+        float yRot = Map(inputHandler.mousePosition.y, 0f, Screen.height, xRodRange.x, xRodRange.y);
+        transform.rotation = Quaternion.Euler(yRot, xRot, transform.rotation.z);
+
+        //Remapping the ranges and setting the position
+        float xLocalPos = Map(inputHandler.mousePosition.x, 0f, Screen.width, xLocalStringRange.x, xLocalStringRange.y);
+        float zLocalPos = Map(inputHandler.mousePosition.y, 0f, Screen.height, zLocalStringRange.x, zLocalStringRange.y);
+        lastPoint.localPosition = new Vector3(xLocalPos, lastPoint.localPosition.y, zLocalPos);
     }
 
 
@@ -64,6 +90,7 @@ public class FishRodHandler : MonoBehaviour
     //Function to remap the ranges 
     float Map(float s, float a1, float a2, float b1, float b2)
     {
+        // Debug.Log("s:" + s + " range1min:" + a1 + " range1max:" + a2 + " range2min:" + b1 + " range2max" + b2);
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
 
